eb698b8117a9b34f5ea79d8d072e89e3149262b8
uranus destroyer <mr.endder55t@mail.ru>
2025-02-17 18:55:30 +0300

    add repositories

---
+++ backend/src/main/java/com/coursework/repository/TransportRepository.java
@@ -0,0 +1,9 @@
+package com.coursework.repository;
+
+import com.coursework.model.Transport;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TransportRepository extends JpaRepository<Transport, Long> {
+}
